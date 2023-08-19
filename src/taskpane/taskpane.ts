af05ef619433767bc472b80d2ec91e6ea22cffff
KiyotakaMatsushita <kiyotaka.86@gmail.com>
2023-08-19 22:00:22 +0900

    Removed 'run' function
    The 'run' function has been removed from taskpane.ts. This function was responsible for handling a click on the 'run' button. This entailed running async operations on Excel, including acquiring a selected range and modifying the fill color. This action appears to be no more required, hence, the removal.

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -5,13 +5,12 @@
 
 import { setAPIKey } from "../util/key";
 
-/* global console, document, Excel, Office */
+/* global console, document, Office */
 
 // The initialize function must be run each time a new page is loaded
 Office.onReady(() => {
   document.getElementById("sideload-msg").style.display = "none";
   document.getElementById("app-body").style.display = "flex";
-  document.getElementById("run").onclick = run;
   document.getElementById("SaveOpenAIApiKey").onclick = saveKey;
 });
 
@@ -24,26 +23,4 @@ export async function saveKey() {
     .catch((error) => {
       console.error("Error saving API key:", error);
     });
-}
-
-export async function run() {
-  try {
-    await Excel.run(async (context) => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
-
-      // Read the range address
-      range.load("address");
-
-      // Update the fill color
-      range.format.fill.color = "yellow";
-
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
 }
