3bacabc0ac9b63f9ef8b63ebfd63038bc7f36bed
KiyotakaMatsushita <kiyotaka.86@gmail.com>
2024-01-09 15:51:50 +0900

    Refactor OpenAI GPT functions for simplicity
    Streamlined the OpenAI GPT-related functions to use default parameters for optional arguments, introducing a new genericGPT function to centralize common logic. Removed redundant validation of model names, relying on revised logic that fetches model names dynamically. Enhanced error logging by adding console.error statements. These changes improve code maintainability and error handling robustness.

--- src/functions/gpt.ts
+++ src/functions/gpt.ts
@@ -1,18 +1,19 @@
 ﻿/* global CustomFunctions */
+/* global console */
 import { convertStringToUnionType } from "../util/typeConverter";
 import { getAPIKey } from "../util/key";
 import { makeConversationContents } from "./core/ChatCompletion/message";
-import { OPENAI_MODEL_NAMES, fetchOpenAICompletion, fetchOpenAIStreamCompletion } from "./core/provider/openai";
+import { getOpenAIModelNames, fetchOpenAICompletion, fetchOpenAIStreamCompletion } from "./core/provider/openai";
 
 /**
  * OpenAI GPT stream chat with system and assistant.
  * @customfunction
- * @param systemPrompt  OpenAI system prompt.
- * @param userPrompt  Last OpenAI user prompt.
- * @param conversationHistory a 2D range of user and assistant conversations.
- * @param model  OpenAI model name.
- * @param maxTokens  OpenAI maxTokens parameter.
- * @param temperature  OpenAI temperature parameter.
+ * @param {systemPrompt}  OpenAI system prompt.
+ * @param {userPrompt} OpenAI user prompt.
+ * @param [conversationHistory] a 2D range of user and assistant conversations.
+ * @param [model] OpenAI model name.
+ * @param [maxTokens] OpenAI maxTokens parameter.
+ * @param [temperature] OpenAI temperature parameter.
  * @param {CustomFunctions.StreamingInvocation<string>} invocation Streaming invocation parameter.
  */
 export function streamGPT(
@@ -24,67 +25,46 @@ export function streamGPT(
   temperature: number,
   invocation: CustomFunctions.StreamingInvocation<string>
 ): void {
-  const m = convertStringToUnionType(model, OPENAI_MODEL_NAMES);
-
-  if (!m) {
-    throw new Error("Invalid model name");
-  }
-
-  getAPIKey().then(async (apiKey) => {
-    const generator = fetchOpenAIStreamCompletion({
-      model: m,
-      maxTokens,
-      temperature,
-      apiKey,
-      systemContent: systemPrompt,
-      userContent: userPrompt,
-      conversationContents: makeConversationContents(conversationHistory),
-    });
-    let tokens = "";
-    for await (const token of generator) {
-      tokens += token;
-      invocation.setResult(tokens);
-    }
-  });
+  genericStreamGPT(
+    userPrompt || "",
+    systemPrompt,
+    conversationHistory || [],
+    model || "gpt-4-0613",
+    maxTokens || 1000,
+    temperature || 0,
+    invocation
+  );
 }
 
 /**
  * OpenAI GPT chat with range of prompts.
- * @customfunction
- * @param systemPrompt  OpenAI system prompt.
- * @param userPrompt  Last OpenAI user prompt.
- * @param conversationHistory a 2D range of user and assistant conversations.
- * @param model  OpenAI model name.
- * @param maxTokens  OpenAI maxTokens parameter.
- * @param temperature  OpenAI temperature parameter.
+ * @customFunction
+ * @param {systemPrompt}  OpenAI system prompt.
+ * @param {userPrompt} OpenAI user prompt.
+ * @param [conversationHistory] a 2D range of user and assistant conversations.
+ * @param [model] OpenAI model name.
+ * @param [maxTokens] OpenAI maxTokens parameter.
+ * @param [temperature] OpenAI temperature parameter.
+ * @return {Promise<string>} OpenAI response.
  */
 export async function GPT(
   systemPrompt: string,
-  userPrompt: string,
-  conversationHistory: string[][],
-  model: string,
-  maxTokens: number,
-  temperature: number
+  userPrompt?: string,
+  conversationHistory?: string[][],
+  model?: string,
+  maxTokens?: number,
+  temperature?: number
 ): Promise<string> {
-  const apiKey = await getAPIKey();
-
-  const m = convertStringToUnionType(model, OPENAI_MODEL_NAMES);
-
-  if (!m) {
-    throw new Error("Invalid model name");
-  }
-
-  const res = await fetchOpenAICompletion({
-    model: m,
-    maxTokens,
-    temperature,
-    apiKey,
-    systemContent: systemPrompt,
-    userContent: userPrompt,
-    conversationContents: makeConversationContents(conversationHistory),
-  });
-  return res.choices[0].message.content;
+  return genericGPT(
+    userPrompt || "",
+    systemPrompt,
+    conversationHistory || [],
+    model || "gpt-4-0613",
+    maxTokens || 1000,
+    temperature || 0
+  );
 }
+
 export async function genericGPT(
   userPrompt: string,
   systemPrompt: string,
@@ -96,15 +76,17 @@ export async function genericGPT(
   try {
     const apiKey = await getAPIKey();
 
-    const m = convertStringToUnionType(model, OPENAI_MODEL_NAMES);
+    console.log(getOpenAIModelNames());
+    // const m = convertStringToUnionType(model, getOpenAIModelNames());
 
-    if (!m) {
-      let error = new CustomFunctions.Error(CustomFunctions.ErrorCode.invalidValue, "Invalid model name");
-      throw error;
-    }
+    // if (!m) {
+    //   let error = new CustomFunctions.Error(CustomFunctions.ErrorCode.invalidValue, "Invalid model name");
+    //   console.error(error);
+    //   throw error;
+    // }
 
     const res = await fetchOpenAICompletion({
-      model: m,
+      model,
       maxTokens,
       temperature,
       apiKey,
@@ -115,6 +97,7 @@ export async function genericGPT(
 
     return res.choices[0].message.content;
   } catch (error) {
+    console.error(error);
     throw new CustomFunctions.Error(CustomFunctions.ErrorCode.notAvailable, error as string | undefined);
   }
 }
@@ -128,16 +111,17 @@ export function genericStreamGPT(
   temperature: number,
   invocation: CustomFunctions.StreamingInvocation<string>
 ): void {
-  const m = convertStringToUnionType(model, OPENAI_MODEL_NAMES);
+  console.log(getOpenAIModelNames());
+  // const m = convertStringToUnionType(model, getOpenAIModelNames());
 
-  if (!m) {
-    let error = new CustomFunctions.Error(CustomFunctions.ErrorCode.invalidValue, "Invalid model name");
-    throw error;
-  }
+  // if (!m) {
+  //   let error = new CustomFunctions.Error(CustomFunctions.ErrorCode.invalidValue, "Invalid model name");
+  //   throw error;
+  // }
 
   getAPIKey().then(async (apiKey) => {
     const generator = fetchOpenAIStreamCompletion({
-      model: m,
+      model,
       maxTokens,
       temperature,
       apiKey,
